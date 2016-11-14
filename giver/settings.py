503d0473c27ed0b03bdaff5fb154a6781b8f6269
palzuncoff <palzuncoff@yahoo.com>
2016-11-14 18:49:21 +0200

    render parser

--- giver/settings.py
+++ giver/settings.py
@@ -146,9 +146,12 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     )
 }
 
