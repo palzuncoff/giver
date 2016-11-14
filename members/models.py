081dfce83b7624401b0f0576bab9d83fc09f7567
palzuncoff <palzuncoff@yahoo.com>
2016-11-14 17:22:45 +0200

    user

--- members/models.py
+++ members/models.py
@@ -14,9 +14,37 @@ from django_facebook.utils import get_profile_model
 
 import logging
 
-from django.contrib.auth.models import User
+logger = logging.getLogger(__name__)
 
-
-
-class Member():
+try:
+    # There can only be one custom user model defined at the same time
+    if getattr(settings, 'AUTH_USER_MODEL', None) == 'member.CustomFacebookUser':
+        from django.contrib.auth.models import AbstractUser, UserManager
+        class CustomFacebookUser(AbstractUser, FacebookModel):
+            '''
+            The django 1.5 approach to adding the facebook related fields
+            '''
+            objects = UserManager()
+            # add any customizations you like
+            state = models.CharField(max_length=255, blank=True, null=True)
+except ImportError as e:
+    logger.info('Couldnt setup FacebookUser, got error %s', e)
     pass
+
+
+
+class Member(FacebookModel):
+    '''
+    Inherit the properties from django facebook
+    '''
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+
+@receiver(post_save)
+def create_profile(sender, instance, created, **kwargs):
+    """Create a matching profile whenever a user object is created."""
+    if sender == get_user_model():
+        user = instance
+        profile_model = get_profile_model()
+        if profile_model == UserProfile and created:
+            profile, new = UserProfile.objects.get_or_create(user=instance)
